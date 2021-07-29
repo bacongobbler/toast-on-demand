312d296e5b4f999fbff683ff75b6ad0293a169ce
itowlson <ivan.towlson@microsoft.com>
2021-07-29 12:29:29 +1200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+  println!("Content-Type: text/html\n");
+  println!("<h1>Welcome to the Toast on Demand web site</h1>");
+  println!("<p>Our mission ia to provide you with delicious and nutritious toast - where you want it, when you want it, and as browned as you want it.</p>");
+  println!("<h2>TOAST OF THE DAY</h2>");
+  println!("<p>Today's special is RAW TOAST, served straight or on the rocks.</p>");
+  println!("<p><img src='assets/images/raw-toast.jpeg'></p>");
+}
