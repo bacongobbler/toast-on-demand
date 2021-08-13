b7a21358ccd579ae9aeac97d6dbea7f35d4d89dd
Matthew Fisher <matt.fisher@microsoft.com>
2021-08-13 11:55:48 -0700

    implement toast randomizer
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 struct Toast {
     name: String,
     description: String,
@@ -5,18 +7,33 @@ struct Toast {
 }
 
 fn main() {
+
+    let toasts: [Toast; 3] = [
+        Toast {
+            name: "RAW TOAST".to_owned(),
+            description: "Served straight or on the rocks.".to_owned(),
+            img_href: "assets/images/raw_toast.jpg".to_owned()
+        },
+        Toast {
+            name: "FRENCH TOAST".to_owned(),
+            description: "pan-fried goodness.".to_owned(),
+            img_href: "assets/images/french_toast.jpg".to_owned()
+        },
+        Toast {
+            name: "PEANUT BUTTER POWER TOAST".to_owned(),
+            description: "peanut butter, banana, and cha seeds. FEEL THE POWER!".to_owned(),
+            img_href: "assets/images/peanut_butter_power_toast.jpg".to_owned()
+        }
+    ];
+
     println!("Content-Type: text/html\n");
     println!("<h1>Welcome to the Toast on Demand web site</h1>");
     println!("<p>Our mission is to provide you with delicious and nutritious toast - where you want it, when you want it, and as browned as you want it.</p>");
 
-    let raw_toast = Toast {
-        name: "RAW TOAST".to_owned(),
-        description: "Served straight or on the rocks.".to_owned(),
-        img_href: "assets/images/raw_toast.jpg".to_owned()
-    };
-
+    let mut rng = thread_rng();
+    let selected_toast = rng.gen_range(0..=2);
     println!("<h2>TOAST OF THE DAY</h2>");
-    println!("<p>Today's special is {}</p>", raw_toast.name);
-    println!("<p>{}</p>", raw_toast.description);
-    println!("<p><img src='{}' /></p>", raw_toast.img_href);
+    println!("<p>Today's special is {}</p>", toasts[selected_toast].name);
+    println!("<p>{}</p>", toasts[selected_toast].description);
+    println!("<p><img src='{}' /></p>", toasts[selected_toast].img_href);
 }
